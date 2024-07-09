63562e6520f4abe1e7fecac35f766f1ee267a784
Jack Westbrook <jack.westbrook@gmail.com>
2024-07-09 19:00:58 +0200

    feat: initial attempt to support timeseries and table dataframes

--- src/hooks/useLightsData.ts
+++ src/hooks/useLightsData.ts
@@ -1,11 +1,15 @@
 import {
   DataFrame,
+  Field,
   FieldType,
+  FieldConfigSource,
   GetFieldDisplayValuesOptions,
+  GrafanaTheme2,
   LinkModel,
   ThresholdsConfig,
   getActiveThreshold,
   getFieldDisplayValues,
+  InterpolateFunction,
 } from '@grafana/data';
 import { useMemo } from 'react';
 import { SortOptions } from 'types';
@@ -48,10 +52,8 @@ type UseLightsData = Omit<GetFieldDisplayValuesOptions, 'reduceOptions'> & {
 
 export function useLightsData(options: UseLightsData): LightsDataResult {
   const { theme, data, fieldConfig, replaceVariables, timeZone, sortLights, reverseColors } = options;
-
   return useMemo(() => {
     let status = LightsDataResultStatus.nodata;
-    let invalidThresholds = undefined;
 
     if (noData(data)) {
       return {
@@ -81,62 +83,137 @@ export function useLightsData(options: UseLightsData): LightsDataResult {
       };
     }
 
-    const fieldDisplayValues = getFieldDisplayValues({
-      fieldConfig: fieldConfig,
-      reduceOptions: { calcs: [] },
-      replaceVariables,
-      theme: theme,
-      data: data,
-      timeZone,
-    });
-
-    const values = fieldDisplayValues.map((displayValue) => {
-      const thresholdsValid = validateThresholds(displayValue.field.thresholds);
-      const activeThreshold = getActiveThreshold(displayValue.display.numeric, displayValue.field.thresholds?.steps);
-      const { title, text, suffix, prefix } = displayValue.display;
-      const thresholdSteps = displayValue.field.thresholds?.steps ?? [];
-      const maybeReversedThresholdSteps = reverseColors ? thresholdSteps.slice().reverse() : thresholdSteps;
-      const colors = maybeReversedThresholdSteps.map((threshold) => {
-        return {
-          color: theme.visualization.getColorByName(threshold.color),
-          active: threshold.value === activeThreshold.value,
-        };
-      });
-
-      const fieldValues =
-        displayValue.view?.dataFrame.fields[1]?.values.toArray() ||
-        displayValue.view?.dataFrame.fields[0]?.values.toArray();
-      const trendValue = basicTrend(fieldValues);
-      const trendColor = theme.visualization.getColorByName(getTrendColor(trendValue));
-
-      if (!thresholdsValid) {
-        status = LightsDataResultStatus.incorrectThresholds;
-        invalidThresholds = displayValue.field.thresholds;
-      } else {
-        status = LightsDataResultStatus.success;
-      }
-
+    if (isTimeSeries(data)) {
+      const [values, lightDataStatus, invalidThresholds] = processTimeSeriesData(
+        fieldConfig,
+        theme,
+        data,
+        replaceVariables,
+        timeZone,
+        reverseColors
+      );
       return {
-        title,
-        value: text,
-        prefix,
-        suffix,
-        colors,
-        trend: {
-          color: trendColor,
-          value: trendValue,
-        },
-        hasLinks: displayValue.hasLinks,
-        getLinks: displayValue.getLinks,
+        values: sortLights === SortOptions.None ? values : sortByValue(values, sortLights),
+        invalidThresholds,
+        status: lightDataStatus,
       };
-    });
+    }
 
+    const [values, lightDataStatus, invalidThresholds] = processTableData(theme, data, reverseColors);
     return {
       values: sortLights === SortOptions.None ? values : sortByValue(values, sortLights),
       invalidThresholds,
-      status,
+      status: lightDataStatus,
     };
   }, [theme, data, fieldConfig, replaceVariables, timeZone, sortLights, reverseColors]);
+}
+
+function processTimeSeriesData(
+  fieldConfig: FieldConfigSource<any>,
+  theme: GrafanaTheme2,
+  data: DataFrame[] | undefined,
+  replaceVariables: InterpolateFunction,
+  timeZone: string | undefined,
+  reverseColors: boolean
+): [LightsDataValues[], LightsDataResultStatus, ThresholdsConfig | undefined] {
+  let lightDataStatus = LightsDataResultStatus.nodata;
+  let invalidThresholds;
+  const fieldDisplayValues = getFieldDisplayValues({
+    fieldConfig: fieldConfig,
+    reduceOptions: { calcs: [] },
+    replaceVariables,
+    theme: theme,
+    data: data,
+    timeZone,
+  });
+
+  const values = fieldDisplayValues.map((displayValue) => {
+    const thresholdsValid = validateThresholds(displayValue.field.thresholds);
+    const activeThreshold = getActiveThreshold(displayValue.display.numeric, displayValue.field.thresholds?.steps);
+    const { title, text, suffix, prefix } = displayValue.display;
+    const thresholdSteps = displayValue.field.thresholds?.steps ?? [];
+    const maybeReversedThresholdSteps = reverseColors ? thresholdSteps.slice().reverse() : thresholdSteps;
+    const colors = maybeReversedThresholdSteps.map((threshold) => {
+      return {
+        color: theme.visualization.getColorByName(threshold.color),
+        active: threshold.value === activeThreshold.value,
+      };
+    });
+
+    const fieldValues =
+      displayValue.view?.dataFrame.fields[1]?.values.toArray() ||
+      displayValue.view?.dataFrame.fields[0]?.values.toArray();
+    const trendValue = basicTrend(fieldValues);
+    const trendColor = theme.visualization.getColorByName(getTrendColor(trendValue));
+
+    if (!thresholdsValid) {
+      lightDataStatus = LightsDataResultStatus.incorrectThresholds;
+      invalidThresholds = displayValue.field.thresholds;
+    } else {
+      lightDataStatus = LightsDataResultStatus.success;
+    }
+
+    return {
+      title,
+      value: text,
+      prefix,
+      suffix,
+      colors,
+      trend: {
+        color: trendColor,
+        value: trendValue,
+      },
+      hasLinks: displayValue.hasLinks,
+      getLinks: displayValue.getLinks,
+    };
+  });
+
+  return [values, lightDataStatus, invalidThresholds];
+}
+
+function processTableData(
+  theme: GrafanaTheme2,
+  data: DataFrame[] | undefined,
+  reverseColors: boolean
+): [LightsDataValues[], LightsDataResultStatus, ThresholdsConfig | undefined] {
+  let lightDataStatus = LightsDataResultStatus.nodata;
+  let invalidThresholds;
+  const numericField = data![0].fields.find((f: Field) => f.type === FieldType.number);
+  const stringField = data![0].fields.find((f: Field) => f.type === FieldType.string);
+  let thresholdsValid = validateThresholds(numericField?.config.thresholds);
+
+  const values = numericField!.values.toArray().map((value, idx) => {
+    const activeThreshold = getActiveThreshold(value, numericField!.config.thresholds?.steps);
+    const { text, suffix, prefix } = numericField!.display!(value);
+    const thresholdSteps = numericField!.config.thresholds?.steps ?? [];
+    const maybeReversedThresholdSteps = reverseColors ? thresholdSteps.slice().reverse() : thresholdSteps;
+    const colors = maybeReversedThresholdSteps.map((threshold) => {
+      return {
+        color: theme.visualization.getColorByName(threshold.color),
+        active: threshold.value === activeThreshold.value,
+      };
+    });
+
+    if (!thresholdsValid) {
+      lightDataStatus = LightsDataResultStatus.incorrectThresholds;
+      invalidThresholds = numericField!.config.thresholds;
+    } else {
+      lightDataStatus = LightsDataResultStatus.success;
+    }
+
+    return {
+      title: stringField!.values.toArray()[idx],
+      value: text,
+      prefix,
+      suffix,
+      colors,
+      trend: { color: 'transparent', value: 0 },
+      hasLinks: value.hasLinks,
+      getLinks: value.getLinks,
+    };
+  });
+
+  return [values, lightDataStatus, invalidThresholds];
 }
 
 function sortByValue(arr: LightsDataValues[], sortOrder: SortOptions): LightsDataValues[] {
@@ -161,6 +238,10 @@ function isSupported(data?: DataFrame[]): boolean {
 
     return Boolean(field);
   });
+}
+
+function isTimeSeries(data?: DataFrame[]): boolean {
+  return Boolean(data?.some((d) => d.fields.some((f) => f.type === FieldType.time)));
 }
 
 function noData(data?: DataFrame[]): boolean {
