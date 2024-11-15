db3cd834c64ebf3ade36638e6442022175885ad4
Jack Westbrook <jack.westbrook@gmail.com>
2024-11-15 17:20:30 +0100

    Tests: Fix failing e2e tests in Grafana 11.4.x (#51)

--- tests/traffic-light.spec.ts
+++ tests/traffic-light.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@grafana/plugin-e2e';
 import { TEST_IDS } from '../src/constants';
 import { Locator } from '@playwright/test';
+import { gte } from 'semver';
 
 test.describe.configure({ mode: 'parallel' });
 
@@ -36,7 +37,7 @@ test('Panel displays a traffic light when thresholds are correctly set', async (
   await expect(trafficLightPanel.getByTestId(TEST_IDS.stop)).toBeVisible();
 });
 
-test('Panel options toggle values component correctly', async ({ panelEditPage, page, selectors }) => {
+test('Panel options toggle values component correctly', async ({ panelEditPage, page, selectors, grafanaVersion }) => {
   await panelEditPage.setVisualization('Traffic Light');
   await page.getByRole('button', { name: /add Threshold/i }).click();
 
@@ -50,24 +51,38 @@ test('Panel options toggle values component correctly', async ({ panelEditPage, 
   const trendColor = await getBackgroundColor(trafficLightValueContainer);
   await expect(trendColor).not.toBe('rgba(0, 0, 0, 0)');
 
-  const showTrendSwitch = panelEditPage
-    .getByGrafanaSelector(selectors.components.PanelEditor.OptionsPane.fieldLabel('Traffic Light Show trend'))
-    .getByLabel('Toggle switch');
-  await showTrendSwitch.uncheck();
+  const trendSwitchLabel = panelEditPage.getByGrafanaSelector(
+    selectors.components.PanelEditor.OptionsPane.fieldLabel('Traffic Light Show trend')
+  );
+  const showTrendSwitch = gte(grafanaVersion, '11.4.0')
+    ? trendSwitchLabel.getByRole('switch')
+    : trendSwitchLabel.getByLabel('Toggle switch');
+
+  await showTrendSwitch.click({ force: true });
 
   const noTrendColor = await getBackgroundColor(trafficLightValueContainer);
   await expect(noTrendColor).toBe('rgba(0, 0, 0, 0)');
 
-  const showValueSwitch = panelEditPage
-    .getByGrafanaSelector(selectors.components.PanelEditor.OptionsPane.fieldLabel('Traffic Light Show value'))
-    .getByLabel('Toggle switch');
-  await showValueSwitch.uncheck();
+  const valueSwitchLabel = panelEditPage.getByGrafanaSelector(
+    selectors.components.PanelEditor.OptionsPane.fieldLabel('Traffic Light Show value')
+  );
+  const showValueSwitch = gte(grafanaVersion, '11.4.0')
+    ? valueSwitchLabel.getByRole('switch')
+    : valueSwitchLabel.getByLabel('Toggle switch');
+
+  await showValueSwitch.click({ force: true });
   await expect(trafficLightPanelValue).not.toBeVisible();
 
-  const showLegendSwitch = panelEditPage
-    .getByGrafanaSelector(selectors.components.PanelEditor.OptionsPane.fieldLabel('Traffic Light Show legend'))
-    .getByLabel('Toggle switch');
-  await showLegendSwitch.uncheck();
+  const legendSwitchLabel = panelEditPage.getByGrafanaSelector(
+    selectors.components.PanelEditor.OptionsPane.fieldLabel('Traffic Light Show legend')
+  );
+
+  const showLegendSwitch = gte(grafanaVersion, '11.4.0')
+    ? legendSwitchLabel.getByRole('switch')
+    : legendSwitchLabel.getByLabel('Toggle switch');
+
+  await showLegendSwitch.click({ force: true });
+
   await expect(trafficLightPanelLegend).not.toBeVisible();
   await expect(trafficLightValueContainer).not.toBeVisible();
 });
@@ -92,7 +107,6 @@ test('Table data: panel displays a traffic light for each row', async ({ panelEd
   await expect(trafficLightPanel.getByTestId(TEST_IDS.go)).toBeVisible();
   const trafficLightPanelValues = await page.getByTestId(TEST_IDS.trafficLightValue).all();
   await expect(trafficLightPanelValues.length).toBe(5);
-
 });
 
 async function getBackgroundColor(el: Locator) {
